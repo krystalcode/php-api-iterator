5784db387ca04108cf99d1103310302b96fd0ff8
Dimitris Bozelos <dbozelos@gmail.com>
2021-11-04 21:34:05 +0000

    Client and Iterator interfaces

---
+++ src/ClientInterface.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace KrystalCode\ApiIterator;
+
+/**
+ * Defines the interface for API clients.
+ *
+ * Normally you would need to implement an adapter for the PHP SDK or other
+ * library that makes the actual calls to the API endpoints so that it
+ * implements this interface. This is necessary for the iterator to know how to
+ * make the API calls for getting the results.
+ */
+interface ClientInterface
+{
+    /**
+     * Gets the list of items from the API resource.
+     *
+     * @param array $options
+     *   An associative array of options. Supported options are:
+     *   - page (int): The index of the page to get, if the resource supports
+     *     paging.
+     *   - limit (int): The number of items to get.
+     *   - bypass_iterator (bool): The items are normally return wrapped in an
+     *     API iterator. When the `bypass_iterator` option is set to, the items
+     *     should be  returned without that extra wrapper iterator i.e. in just
+     *     a `\CachingIterator`.
+     * @param array $query
+     *   An associative array containing additional query parameters to add to
+     *   the request.
+     *
+     * @return \KrystalCode\ApiIterator\IteratorInterface|array|\CachingIterator
+     *   - If the endpoint supports paging, an API iterator containing the
+     *     items.
+     *   - If the `bypass_iterator` option is set to `true`, an array containing
+     *     the following elements in the given order.
+     *     - \CachingIterator: An iterator containing the list items; items are
+     *       `stdClass` objects.
+     *     - int|null|false: The total number of pages, NULL if unknown, or
+     *       FALSE if unknown but it is know that we have reached the last page.
+     *     - array: The updated query array. This may be used to update the
+     *       token or the URL that will be used to get the next page.
+     *   - If the endpoint does not support paging, a `\CachingIterator`
+     *     iterator containing the list items.
+     *
+     * @throws \InvalidArgumentException
+     *   If options related to paging are set but the resource does not support
+     *   paging.
+     */
+    public function list(array $options = [], array $query = []);
+
+}
---
+++ src/IteratorInterface.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace KrystalCode\ApiIterator;
+
+/**
+ * Defines the interface for API iterators.
+ *
+ * API iterators facilitate getting results from resource endpoints that list
+ * items with support for pagination. It also supports caching i.e. browsing
+ * pages that are already retrieved without making new API calls.
+ */
+interface IteratorInterface extends \Iterator
+{
+    /**
+     * Sets the current position i.e. page index of the iterator.
+     *
+     * @param int $pageIndex
+     *   The page index to set the current position to.
+     */
+    public function setKey(int $pageIndex): void;
+
+    /**
+     * Gets the total number of pages for the iterator.
+     *
+     * The total number of pages is generally not known before getting the items
+     * for at least one page from the API. Other APIs do not provide the total
+     * number of pages in the response; instead they provide a token or a link
+     * to the next page. In such cases we do not know the total number of pages
+     * until we iterate through all of them. This method returns `null` until
+     * the count is known.
+     *
+     * @return int|null
+     *   The total number of pages, or `null` if it is not known yet.
+     */
+    public function count(): ?int;
+
+    /**
+     * Sets the total number of pages for the iterator.
+     *
+     * @param int $nbPages
+     *   The total number of pages.
+     */
+    public function setCount(int $nbPages): void;
+
+    /**
+     * Gets whether caching is enabled or not.
+     *
+     * @return bool
+     *   True if caching is enabled, false otherwise.
+     */
+    public function cache(): bool;
+
+    /**
+     * Enables or disables caching.
+     *
+     * @param bool $cache
+     *   True for enabling caching, false for disabling.
+     */
+    public function setCache(bool $cache): void;
+
+    /**
+     * Gets the items for the requested page.
+     *
+     * It moves the position to the requested page index and fetches the
+     * items.
+     *
+     * @param int $pageIndex
+     *   The index of the page for which to get the results.
+     *
+     * @return \CachingIterator
+     *   An iterator containing the items.
+     *
+     * @throws \InvalidArgumentException
+     *   If the given page index is an invalid position.
+     */
+    public function get(int $pageIndex): \CachingIterator;
+
+    /**
+     * Moves the position to the requested page.
+     *
+     * @param int $pageIndex
+     *   The index of the page to move the position to.
+     *
+     * @throws \InvalidArgumentException
+     *   If the given page index is invalid.
+     */
+    public function move(int $pageIndex): void;
+}
